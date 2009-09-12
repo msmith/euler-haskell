fb2292910eb29a8369c19c1287294e677d35d935
Mike Smith <msmith@sticknet.net>
2009-09-11 23:41:12 -0700

    28

---
+++ P28.hs
@@ -0,0 +1,13 @@
+module P28 (euler28) where
+    
+euler28 = solve 1001
+
+solve n = sum $ takeWhile (<=(n^2)) series
+
+series = 1:(f 1 skips)
+    where
+        skips = concat $ map (take 4 . repeat) $ iterate (+2) 2
+        f x ss = n:(f n ns)
+            where
+                (s:ns) = ss
+                n = x + s
