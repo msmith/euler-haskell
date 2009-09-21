7c7b805a6da119296c9fe32b12d8a7e3ba0f9005
Mike Smith <msmith@sticknet.net>
2009-09-20 22:51:55 -0700

    faster 45

--- P45.hs
+++ P45.hs
@@ -1,11 +1,17 @@
-module P45 where
+module P45 (euler45) where
 
-euler45 = head $ dropWhile (<=40755) [x | x <- hexs, (find tris x) && (find pents x)]
+euler45 = (series tris pents hexs)!!2
     where
-        find ns n = n == (head $ dropWhile (< n) ns)
         tris  = map tri [1..]
         pents = map pent [1..]
         hexs  = map hex [1..]
-        tri n  = n*(n+1) `div` 2
-        pent n = n*(3*n-1) `div` 2
-        hex n  = n*(2*n-1)
+
+tri n  = n*(n+1) `div` 2
+pent n = n*(3*n-1) `div` 2
+hex n  = n*(2*n-1)
+
+series (a:as) (b:bs) (c:cs)
+    | a == b && a == c = a:(series as bs cs)
+    | a < b || a < c   = series as (b:bs) (c:cs)
+    | b < a || b < c   = series (a:as) bs (c:cs)
+    | c < a || c < b   = series (a:as) (b:bs) cs
