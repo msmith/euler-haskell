a1e0b699ec866402d44e4d48688ff3a188cda766
Mike Smith <msmith@sticknet.net>
2009-09-21 23:54:28 -0700

    39

---
+++ P39.hs
@@ -0,0 +1,18 @@
+import Data.List (nub, maximumBy)
+import Data.Ord (comparing)
+
+combinations p = [t |
+    a <- [1..p `div` 3],
+    b <- [a..p-a],
+    c <- [p-a-b],
+    let t = (a,b,c),
+    a + b + c == p,
+    rightTriangle t]
+
+rightTriangle (a,b,c) = a*a + b*b == c*c
+
+solve p = maximumBy (comparing snd) $ map (\p -> (p, length $ combinations p)) [1..p]
+
+euler39 = fst $ solve 1000
+
+main = do print euler39
